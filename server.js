f466eac9c231fcb8adea20c7ffe527a07eeac406
Eduard Babetskyi <edward.babetskyi@gmail.com>
2024-10-06 14:11:18 +0300

    fix bugs

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const paymentController = require('./routes/pay.route')
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+// });
 
 app.use(express.json());
 app.use(bodyParser.json());
